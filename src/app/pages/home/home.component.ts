f1e78b68b72bf7bffbc9781ee697be7b39b7f425
SrTetbald <dev.hebert.abc@gmail.com>
2025-11-17 22:34:33 -0300

    Mudança para meu servidor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,8 +11,5 @@ import { FooterComponent } from '../../components/footer/footer.component';
     styleUrl: './home.component.css',
 })
 export class HomeComponent {
-    ngOnInit(): void {
-        alert(`Olá seja bem vindo.
-Esse projeto é apenas um projeto de estudo. Sendo assim a hospedagem é gratuita e pode apresentar instabilidades, se for a primeira vez abrindo a página aguarde uns segundos até que a API carregue no servidor.`);
-    }
+    
 }
